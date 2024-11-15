af599691caeeb8148d2fd33fc3ea4759de825269
sherlynkek <sherlynkek00@gmail.com>
2024-11-15 09:11:13 +0800

    README.md

---
+++ src/test/java/sg/edu/nus/iss/vttp5a_day2l/Vttp5aDay2lApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5a_day2l;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5aDay2lApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
